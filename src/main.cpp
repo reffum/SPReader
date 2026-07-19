5b75bd4e6621c395e53bd5d490359302eeb8af6d
Oleg Gavrilchenko <oleggavrilcenko6@gmail.com>
2026-07-19 23:57:30 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QLabel>
+
+#include "MainWindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    return QApplication::exec();
+}
