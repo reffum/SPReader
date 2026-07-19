e582c55bc8f0f5bcfdee8db7b4051d60f2c5d7ab
Oleg Gavrilchenko <oleggavrilcenko6@gmail.com>
2026-07-19 23:57:30 +0300

    Initial commit

---
+++ src/MainWindow.h
@@ -0,0 +1,16 @@
+//
+// Created by oleg on 7/20/26.
+//
+
+#ifndef SPREADER_MAINWINDOW_H
+#define SPREADER_MAINWINDOW_H
+#include <qt/QtWidgets/QMainWindow>
+
+#include "ui/ui_mainwindow.h"
+
+class MainWindow : public QMainWindow, Ui::MainWindow
+{
+	Q_OBJECT
+};
+
+#endif //SPREADER_MAINWINDOW_H
